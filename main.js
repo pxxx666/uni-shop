ffb94bcc0388e195ced9c9da8e2c9cfce1229105
PX <1209304680@qq.com>
2023-10-16 16:02:16 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ import {$http} from '@escook/request-miniprogram'
 
 uni.$http = $http
 
+$http.baseUrl = 'https://www.uinav.com'
+
 $http.beforeRequest = function(options){
 	uni.showLoading({
 		title:"数据加载中..."
@@ -15,6 +17,14 @@ $http.beforeRequest = function(options){
 $http.afterRequest = function(){
 	uni.hideLoading()
 }
+uni.$showMsg = function(title = '数据请求失败!', duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
 
 Vue.config.productionTip = false
 
